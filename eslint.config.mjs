a158a2c53e9dbbd1578138a76740d7ebc188d064
Renato Lacerda <renato.ac.lacerda@gmail.com>
2024-12-23 17:26:04 -0300

    chore: removing eslint rules for max attr per line

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,7 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt({
   rules: {
-    "vue/max-attributes-per-line": [
-      "error",
-      {
-        singleline: {
-          max: 3,
-        },
-      },
-    ],
+    "vue/max-attributes-per-line": ["off"],
     "vue/html-self-closing": [
       "error",
       {

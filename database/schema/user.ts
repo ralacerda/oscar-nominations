fa907732e9f46a64c9a12249b4d320068f571fda
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-01-04 11:22:32 -0300

    chore: references movie on movieStatus

--- database/schema/user.ts
+++ database/schema/user.ts
@@ -1,5 +1,6 @@
 import { relations } from "drizzle-orm";
 import { int, sqliteTable, text, unique } from "drizzle-orm/sqlite-core";
+import { movies } from "./movies";
 
 export const users = sqliteTable(
   "users",
@@ -24,7 +25,9 @@ export const movieStatus = sqliteTable("movie_status", {
   userId: int()
     .notNull()
     .references(() => users.id),
-  movieId: int().notNull(),
+  movieId: int()
+    .notNull()
+    .references(() => movies.id),
   status: text({ enum: ["watchlist", "watched", "recommended"] }).notNull(),
 });
 
@@ -32,6 +35,10 @@ export const movieStatusRelation = relations(movieStatus, ({ one }) => ({
   user: one(users, {
     fields: [movieStatus.userId],
     references: [users.id],
+  }),
+  movie: one(movies, {
+    fields: [movieStatus.movieId],
+    references: [movies.id],
   }),
 }));
 

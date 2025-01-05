289f34fa81a975d6739591cfc5d0e1aee36c61a5
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-01-05 19:46:24 -0300

    chore: improve the way the users email is unique

--- database/schema/user.ts
+++ database/schema/user.ts
@@ -1,24 +1,13 @@
 import { relations } from "drizzle-orm";
-import { int, sqliteTable, text, unique } from "drizzle-orm/sqlite-core";
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { movies } from "./movies";
 
-export const users = sqliteTable(
-  "users",
-  {
-    id: int().primaryKey(),
-    // @ts-expect-error - Weird type bug because of the third argument (function to declare unique constraints)
-    name: text().notNull(),
-    picture: text().notNull(),
-    email: text().notNull(),
-  },
-  (table) => {
-    return [
-      {
-        unq: unique().on(table.email),
-      },
-    ];
-  },
-);
+export const users = sqliteTable("users", {
+  id: int().primaryKey(),
+  name: text().notNull(),
+  picture: text().notNull(),
+  email: text().notNull().unique(),
+});
 
 export const movieStatus = sqliteTable("movie_status", {
   id: int().primaryKey(),
